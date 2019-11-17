7c43f16f072e070e09633701a922a846f908f56b
Daniel Gordon <d_gordon1@icloud.com>
2019-11-18 10:04:37 +1100

    finished devise lecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  # Check if there is a user logged in based on the session
+  # If no current_user -> redirect to the login page.
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
